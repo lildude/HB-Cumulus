a4e050b9c365a3eec29371133b76b5fdac9f6656
Colin Seymour <lildood@gmail.com>
2008-12-06 11:23:05 +0000

    Tidied up configure panel code

--- hb-cumulous.plugin.php
+++ hb-cumulous.plugin.php
@@ -2,9 +2,12 @@
 /**
  * This is a direct port of WP-Cumulus available from http://www.roytanck.com/2008/03/15/wp-cumulus-released
  *
- * TODO:
- *  - Create my own FormControls - cos the defaults are quite pants.
+ * TODO: Create my own FormControls - cos the defaults are quite pants.
+ * TODO: Use some of the tagcloud plugin code to get list of tags and usage etc for cloud
  */
+// Needed so my controls work.  Probably need to rather use Theme::create() instead.
+new FormUI('dummy');
+
 class HbCumulus extends Plugin {
     public function info() {
         return array (
@@ -71,41 +74,35 @@ class HbCumulus extends Plugin {
 				case _t('Configure'):
                     $options = unserialize(Options::get('hb-cumulus__options_' . User::identify()->id));
 					$ui= new FormUI( strtolower( get_class( $this ) ) );
-                    $ui->append('fieldset', 'appearance', _t('Appearance'));
-                        $ui->appearance->append( 'hidden', 'option_mode', 'null:null');
-                            $ui->appearance->option_mode->value = $options['mode'];
-                        $ui->appearance->append( 'text', 'options_width', 'null:null', _t('Width of Flash Tag Cloud (px)'), 'optionscontrol_text');
-                            $ui->appearance->options_width->value = $options['width'];
-                        $ui->appearance->append( 'text', 'options_height', 'null:null', _t('Height of Flash Tag Cloud (px)'), 'optionscontrol_text');
-                            $ui->appearance->options_height->value = $options['height'];
-                        $ui->appearance->append( 'text', 'options_tcolor', 'null:null', _t('Color of the Tags'), 'optionscontrol_text');
-                            $ui->appearance->options_tcolor->value = $options['tcolor'];
-                        $ui->appearance->append( 'text', 'options_tcolor2', 'null:null', _t('Second Color for Gradient (opt)'), 'optionscontrol_text');
-                            $ui->appearance->options_tcolor2->value = $options['tcolor2'];
-                        $ui->appearance->append( 'text', 'options_hicolor', 'null:null', _t('Highlight Color (opt)'), 'optionscontrol_text');
-                            $ui->appearance->options_hicolor->value = $options['hicolor'];
-                        $ui->appearance->append( 'text', 'options_bgcolor', 'null:null', _t('Background Color'), 'optionscontrol_text');
-                            $ui->appearance->options_bgcolor->value = $options['bgcolor'];
-                        $ui->appearance->append( 'text', 'options_speed', 'null:null', _t('Rotation Speed'), 'optionscontrol_text');
-                            $ui->appearance->options_speed->value = $options['speed'];
-                        //$ui->appearance->append( 'text', 'options_trans', 'null:null', _t('Use Transparent Mode'), 'optionscontrol_text');
-                        //    $ui->appearance->options_trans->value = $options['trans'];
-                        $ui->appearance->append( 'select', 'options_trans', 'null:null', _t( 'Use Transparent Mode' ));
-                            $ui->appearance->options_trans->template = 'optionscontrol_select';
-                            $ui->appearance->options_trans->options = array('true' => 'True', 'false' => 'False');
-                        //$ui->appearance->append( 'text', 'options_distr', 'null:null', _t('Distribute Tags Evenly on Sphere'), 'optionscontrol_text');
-                        //    $ui->appearance->options_distr->value = $options['distr'];
-                        $ui->appearance->append( 'select', 'options_distr', 'null:null', _t( 'Distribute Tags Evenly on Sphere' ));
-                            $ui->appearance->options_distr->template = 'optionscontrol_select';
-                            $ui->appearance->options_distr->options = array('true' => 'True', 'false' => 'False');
-                        //$ui->appearance->append( 'text', 'options_args', 'null:null', _t('Parameter String for $tags'), 'optionscontrol_text');
-                        //    $ui->appearance->options_args->value = $options['args'];
-                          
+                        $ui->append( 'hidden', 'option_mode', 'null:null');
+                            $ui->option_mode->value = $options['mode'];
+                        $ui->append( 'trtext', 'options_width', 'null:null', _t('Width of Flash Tag Cloud (px)'));
+                            $ui->options_width->value = $options['width'];
+                        $ui->append( 'text', 'options_height', 'null:null', _t('Height of Flash Tag Cloud (px)'), 'optionscontrol_text');
+                            $ui->options_height->value = $options['height'];
+                        $ui->append( 'text', 'options_tcolor', 'null:null', _t('Color of the Tags'), 'optionscontrol_text');
+                            $ui->options_tcolor->value = $options['tcolor'];
+                        $ui->append( 'text', 'options_tcolor2', 'null:null', _t('Second Color for Gradient (opt)'), 'optionscontrol_text');
+                            $ui->options_tcolor2->value = $options['tcolor2'];
+                        $ui->append( 'text', 'options_hicolor', 'null:null', _t('Highlight Color (opt)'), 'optionscontrol_text');
+                            $ui->options_hicolor->value = $options['hicolor'];
+                        $ui->append( 'text', 'options_bgcolor', 'null:null', _t('Background Color'), 'optionscontrol_text');
+                            $ui->options_bgcolor->value = $options['bgcolor'];
+                        $ui->append( 'text', 'options_speed', 'null:null', _t('Rotation Speed'), 'optionscontrol_text');
+                            $ui->options_speed->value = $options['speed'];
+                        $ui->append( 'select', 'options_trans', 'null:null', _t( 'Use Transparent Mode' ));
+                            $ui->options_trans->template = 'optionscontrol_select';
+                            $ui->options_trans->options = array('true' => 'True', 'false' => 'False');
+                        $ui->append( 'select', 'options_distr', 'null:null', _t( 'Distribute Tags Evenly on Sphere' ));
+                            $ui->options_distr->template = 'optionscontrol_select';
+                            $ui->options_distr->options = array('true' => 'True', 'false' => 'False');
+
+                          /* Would only need this if the TagCloud plugin was used.
                     $ui->append('fieldset', 'advanced', _t('Advanced Options'));
                         $ui->advanced->append( 'label', '', _t('Please leave this setting empty unless you know what you\'re doing.'));
                         $ui->advanced->append( 'text', 'options_args', 'null:null', _t('Parameter String for $tags'), 'optionscontrol_text');
-                            $ui->advanced->options_width->value = $options['args'];
-
+                            $ui->advanced->options_args->value = $options['args'];
+                           */
 					$ui->append( 'submit', 'save', _t('Save Options') );
                     $ui->on_success( array($this, 'serializeNStoreOpts') );
 					$ui->out();
@@ -120,9 +117,10 @@ class HbCumulus extends Plugin {
 
      static function serializeNStoreOpts ($ui) {
         $newOptions = array();
-        foreach($ui->appearance->controls as $option) {
-            list($a, $name) = explode('_', $option->name);
-            $newOptions[$name] = $option->value;
+        foreach($ui->controls as $option) {
+            if ($option->name == 'save') continue;
+           list($a, $name) = explode('_', $option->name);
+           $newOptions[$name] = $option->value;
         }
         Options::set('hb-cumulus__options_' . User::identify()->id, serialize($newOptions));
      }
@@ -142,5 +140,20 @@ HB_CUMULUS_CSS;
 		}
     }
 }
+
+/**
+ * Testing creating my own from controls.  I like things to be uniform and easy to style
+ * The formcontrols_* and optionscontrols_* templates are a bit crap and don't provide
+ * a table based mechanism, which this function does.
+ * TODO: Find a better way of doing this
+ */
+
+class FormControlTrtext extends FormControl {
+  function get($forvalidation = true) {
+    return '<tr><th>'.$this->caption.'</th><td><input type="text" name="'.$this->field.'" value="'.$this->value.'"></td></tr>';
+  }
+}
+
+
 
 ?>
